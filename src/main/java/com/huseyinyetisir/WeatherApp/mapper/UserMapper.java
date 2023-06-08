e21159af2f57cee9e8b016ef364d0506d2ddb7da
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-06-08 21:33:00 +0300

    Deleted Password field in dto

--- src/main/java/com/huseyinyetisir/WeatherApp/mapper/UserMapper.java
+++ src/main/java/com/huseyinyetisir/WeatherApp/mapper/UserMapper.java
@@ -1,22 +1,26 @@
 package com.huseyinyetisir.WeatherApp.mapper;
 
-
 import com.huseyinyetisir.WeatherApp.dto.user.UserDTO;
 import com.huseyinyetisir.WeatherApp.dto.user.UserSaveRequest;
-import com.huseyinyetisir.WeatherApp.dto.user.UserUpdateRequest;
-import com.huseyinyetisir.WeatherApp.entity.User;
+import com.huseyinyetisir.WeatherApp.dto.user.UserUpdatePasswordRequest;
+import com.huseyinyetisir.WeatherApp.entity.AppUser;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
-    
+
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    User convertToUser(UserSaveRequest request);
-    
-    UserDTO convertToUserDTO(User user);
+    AppUser convertToUser(UserSaveRequest request);
 
-    User convertToUser(UserUpdateRequest request);
+    UserDTO convertToUserDTO(AppUser appUser);
+
+    AppUser convertToUser(UserUpdatePasswordRequest request);
+
+    List<UserDTO> convertToUserDtoList(List<AppUser> userList);
+
 }
